5c7b2c4bbc50875483ff95313858902a9c7d6fde
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-13 19:30:26 +0200

    start Graph header file

---
+++ include/Graph.hpp
@@ -0,0 +1,41 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP 
+
+#include <vector>
+#include <cstdint>
+
+
+template <typename T>
+concept Arithmetic = std::is_arithmetic_v<T>;
+
+template <Arithmetic T>
+class Graph<T> {
+	private:
+		std::vector< std::vector<T> > paths_weights_; 
+		uint16_t num_nodes_ = 0;
+
+	public:
+		
+		// Constructors
+		
+		Graph() = default;
+
+		Graph(const uint16_t num_nodes);
+
+
+		// consult methods
+		uint16_t num_nodes() const noexcept;
+
+		bool is_connected(const uint16_t first_node, const uint16_t second_node) const noexcept;
+		
+
+		// paths modification methods
+		void connect(const uint16_t first_node, const uint16_t second_node, const T cost) noexcept;
+
+		void disconnect(const uint16_t first_node, const uint16_t second_node) noexcept;
+
+};
+
+#include "src/Graph.tpp"
+
+#endif
