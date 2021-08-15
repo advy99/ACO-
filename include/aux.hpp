bb0e49df0100ef0aad5391ec660a069bf8fde8ee
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-15 23:59:10 +0200

    SDL + std headers in aux

--- include/aux.hpp
+++ include/aux.hpp
@@ -1,6 +1,12 @@
 #ifndef AUX_HPP
 #define AUX_HPP 
 
+#include <cstdint>
+#include <memory>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+
 template <typename T>
 concept floating_point = std::is_floating_point_v<T>;
 
