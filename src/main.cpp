563da04efeb21888a818408f2cba1e012752a518
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-15 18:22:46 +0200

    main file calling the interface, at this moment does nothing, not even close

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include "ACOppInterface.hpp"
+
+int main() {
+
+	ACOppInterface interface;
+
+	const uint32_t FPS = 60;
+	const uint32_t DELAY_TIME = 1000.0 / FPS;
+
+	uint32_t frame_start, frame_time;
+
+	interface.init("Prueba SDL", 100, 100, 640, 480, 0);
+
+	while ( interface.running() ) {
+		frame_start = SDL_GetTicks();
+
+		frame_time = SDL_GetTicks() - frame_start;
+
+		if (frame_time < DELAY_TIME ) {
+			SDL_Delay(static_cast<int> (DELAY_TIME - frame_time));
+		}
+	}
+
+
+	return 0;
+}
+
