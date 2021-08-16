092a579c01172adc310c218bf762191c7fcd2371
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-16 17:45:36 +0200

    the Rectangle is a Figure

--- include/Rectangle.hpp
+++ include/Rectangle.hpp
@@ -2,19 +2,12 @@
 #define RECTANGLE_HPP 
 
 #include "aux.hpp"
-#include "Color.hpp"
+#include "Figure.hpp"
 
-class Rectangle {
+class Rectangle : public Figure {
 	private:
-		int16_t x_position_;
-		int16_t y_position_;
-
 		int16_t width_;
 		int16_t height_;
-
-		Color color_;
-
-		bool filled_;
 
 	public:
 
@@ -22,28 +15,13 @@ class Rectangle {
 					 const int16_t width, const int16_t height,
 					 const Color & color = {255, 255, 255, 255}, const bool filled = true);
 
-		void draw(std::shared_ptr<SDL_Renderer> renderer);
+		virtual void draw(std::shared_ptr<SDL_Renderer> renderer) override;
 		
-		int16_t x() const noexcept;
-		int16_t y() const noexcept;
-
 		int16_t width() const noexcept;
 		int16_t height() const noexcept;
 
-		Color color() const noexcept;
-
-		bool filled() const noexcept;
-
-		void set_x(const int16_t x) noexcept;
-		void set_y(const int16_t y) noexcept;
-
 		void set_width(const int16_t width) noexcept;
 		void set_height(const int16_t height) noexcept;
-
-		void set_color(const uint8_t red, const uint8_t green, const uint8_t blue, const uint8_t alpha = 255) noexcept;
-		void set_color(const Color & color) noexcept;
-
-		void set_filled(const bool filled) noexcept;
 
 };
 
--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -3,8 +3,7 @@
 Rectangle :: Rectangle (const int16_t x, const int16_t y,
 								const int16_t width, const int16_t height,
 								const Color & color, const bool filled)
-	:x_position_(x), y_position_(y), width_(width), height_(height),
-	 color_(color), filled_(filled)
+	: Figure(x, y, color, filled), width_(width), height_(height)
 {
 }
 
@@ -16,14 +15,6 @@ void Rectangle :: draw(std::shared_ptr<SDL_Renderer> renderer) {
 	}
 }
 
-int16_t Rectangle :: x() const noexcept {
-	return x_position_;
-}
-
-int16_t Rectangle :: y() const noexcept {
-	return y_position_;
-}
-
 int16_t Rectangle :: width() const noexcept {
 	return width_;
 }
@@ -32,42 +23,3 @@ int16_t Rectangle :: height() const noexcept {
 	return height_;
 }
 
-Color Rectangle :: color() const noexcept {
-	return color_;
-}
-
-bool Rectangle :: filled() const noexcept {
-	return filled_;
-}
-
-void Rectangle :: set_x(const int16_t x) noexcept {
-	x_position_ = x;
-}
-
-void Rectangle :: set_y(const int16_t y) noexcept {
-	y_position_ = y;
-}
-
-void Rectangle :: set_width(const int16_t width) noexcept {
-	width_ = width;
-}
-
-void Rectangle :: set_height(const int16_t height) noexcept {
-	height_ = height;
-}
-
-
-void Rectangle :: set_color(const uint8_t red, const uint8_t green, const uint8_t blue, const uint8_t alpha) noexcept {
-	color_.set_red(red);
-	color_.set_green(green);
-	color_.set_blue(blue);
-	color_.set_alpha(alpha);
-}
-
-void Rectangle :: set_color(const Color & color) noexcept {
-	color_ = color;
-}
-
-void Rectangle :: set_filled(const bool filled) noexcept {
-	filled_ = filled;
-}
