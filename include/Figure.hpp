cf753f9d1d8b2984957fee591636f8d2e4de85ef
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-16 17:45:22 +0200

    create Figure class from all the basics for a Figure

---
+++ include/Figure.hpp
@@ -0,0 +1,41 @@
+#ifndef FIGURE_HPP
+#define FIGURE_HPP
+
+#include "aux.hpp"
+#include "Color.hpp"
+
+class Figure {
+	protected:
+
+		int16_t x_position_;
+		int16_t y_position_;
+
+		Color color_;
+
+		bool filled_;
+
+		Figure(const int16_t x, const int16_t y, const Color & color, const bool filled);
+	
+	public:
+
+		virtual void draw(std::shared_ptr<SDL_Renderer> renderer) = 0;
+
+		int16_t x() const noexcept;
+		int16_t y() const noexcept;
+
+		Color color() const noexcept;
+
+		bool filled() const noexcept;
+
+		void set_x(const int16_t x) noexcept;
+		void set_y(const int16_t y) noexcept;
+
+		void set_color(const uint8_t red, const uint8_t green, const uint8_t blue, const uint8_t alpha = 255) noexcept;
+		void set_color(const Color & color) noexcept;
+
+		void set_filled(const bool filled) noexcept;
+
+};
+
+
+#endif 
---
+++ src/Figure.cpp
@@ -0,0 +1,50 @@
+#include "Figure.hpp"
+
+
+Figure :: Figure (const int16_t x, const int16_t y,
+						const Color & color, const bool filled)
+	:x_position_(x), y_position_(y), color_(color), filled_(filled)
+{
+}
+
+
+int16_t Figure :: x() const noexcept {
+	return x_position_;
+}
+
+int16_t Figure :: y() const noexcept {
+	return y_position_;
+}
+
+
+Color Figure :: color() const noexcept {
+	return color_;
+}
+
+bool Figure :: filled() const noexcept {
+	return filled_;
+}
+
+void Figure :: set_x(const int16_t x) noexcept {
+	x_position_ = x;
+}
+
+void Figure :: set_y(const int16_t y) noexcept {
+	y_position_ = y;
+}
+
+
+void Figure :: set_color(const uint8_t red, const uint8_t green, const uint8_t blue, const uint8_t alpha) noexcept {
+	color_.set_red(red);
+	color_.set_green(green);
+	color_.set_blue(blue);
+	color_.set_alpha(alpha);
+}
+
+void Figure :: set_color(const Color & color) noexcept {
+	color_ = color;
+}
+
+void Figure :: set_filled(const bool filled) noexcept {
+	filled_ = filled;
+}
