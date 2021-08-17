76fec9325a85cd5a957e0b4e40cd803b845ec980
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-17 23:31:29 +0200

    more methods for Ant

--- include/Ant.hpp
+++ include/Ant.hpp
@@ -8,11 +8,18 @@ class Ant {
 	private:
 		uint32_t position_;
 
-		const double node_visibility_;
-		const double pheromones_deposited_;
+		// all ants will see the same nodes and deposit the same number of pheromones
+		static double node_visibility_;
+		static double pheromones_deposited_;
 		
 
 	public:
+
+		explicit Ant(const uint32_t position = 0);
+
+		Ant(const Ant & other) = default;
+
+		~Ant() = default;
 
 		uint32_t select_path(const Graph<double> & paths, const Graph<double> & pheromones) const noexcept;
 
@@ -21,6 +28,17 @@ class Ant {
 		void update_position(const uint32_t new_position) noexcept;
 		
 		uint32_t position() const noexcept;
+
+
+		static double visibility() const noexcept;
+
+		static double pheromones_deposited() const noexcept;
+
+		static void set_visibility(const double visibility) noexcept;
+
+		static void set_pheromones_deposited(const double pheromones_per_ant) noexcept;
+
+		Ant & operator = (const Ant & other) = default; 
 
 };
 
