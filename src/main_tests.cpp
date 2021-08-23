6d39674d2f030f369779d846f0ce6eaf65f22b13
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-08-23 19:27:26 +0200

    add main for tests

---
+++ src/main_tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "tests/tests_graph.hpp"
+
+
+#include <string>
+
+int main(int argc, char ** argv) {
+
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
