993c7850a7078a9844c8a579e67d7fe42588207c
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-01 20:02:26 +0200

    Read TSP problem from matriz

--- src/ACOppCLI.cpp
+++ src/ACOppCLI.cpp
@@ -1,37 +1,90 @@
 #include "AntColony.hpp"
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+Graph<double> read_from_file(std::string file) {
+
+	std::ifstream f(file);
+
+	std::string line;
+
+	std::getline(f, line);
+
+	uint32_t num_line = 0;
+	uint32_t num_cities = 0;
+	
+	double value;
+	std::vector<double> first_line;
+	std::istringstream iss_f(line);
+
+	while (!iss_f.eof()) {
+		iss_f >> value;
+		first_line.push_back(value);
+	}
+
+	num_cities = first_line.size();
+
+	Graph<double> solution(num_cities, 0);
+
+	for (unsigned int i = 0; i < first_line.size(); i++) {
+		solution.connect(0, i, first_line[i]);
+	}
+
+	std::getline(f, line);
+	num_line++;
+	
+	uint32_t num_val = 0;
+
+	while(!f.eof()) {	
+		std::istringstream iss(line);
+		num_val = 0;
+		while (!iss.eof()) {
+			iss >> value;
+			if (num_val > num_line) {
+				solution.connect(num_line, num_val, value);
+			}
+			num_val++;
+		}
+
+			
+		std::getline(f, line);
+		num_line++;
+	}
+
+	return solution;
+}
+
 
 int main (int argc, char ** argv) {
 	
-	if ( argc < 3) {
+	if ( argc < 4) {
 		std::cerr << "ERROR: Incorrect number of parameters.\n"
-					 << "\t Use: " << argv[0] << " <num_ants> <num_iterations>\n";
+					 << "\t Use: " << argv[0] << " <num_ants> <num_iterations> <file>\n";
 
 		std::exit(-1);
 	}
 
 	int num_ants = std::atoi(argv[1]);
 	int iterations = std::atoi(argv[2]);
+	std::string file = std::string(argv[3]);
 
-	int num_cities = 6;
 
-	Graph<double> paths(num_cities);
 
-	for ( int32_t i = 0; i < num_cities; i++) {
-		for (int32_t j = i + 1; j < num_cities; j++) {
-			paths.connect(i, j, Random::next_float(10.0, 500.0));
-		}
-	}
+	Graph<double> paths = read_from_file(file);
 
-	std::cout << "Paths: \n\n";
-	for ( int32_t i = 0; i < num_cities; i++) {
-		for (int32_t j = i; j < num_cities; j++) {
+	std::cout << "\nPaths: \n\n";
+	for ( uint32_t i = 0; i < paths.num_nodes(); i++) {
+		for ( uint32_t j = i; j < paths.num_nodes(); j++) {
 			std::cout << paths.cost(i, j) << " ";
 		}
 		std::cout << "\n";
 	}
+	
+	std::cout << std::endl;
 
+	std::cout << "Creating colony and simulating: \n";
 	AntColony my_colony(paths, num_ants);
 
 	auto solution = my_colony.run_simulation(iterations);
