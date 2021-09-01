3a04bf2689e0777e2fc5324693a758f1a0cb737a
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-01 12:45:44 +0200

    AntColony: local update pheromones parameters and calculate initial pheromones using a greedy algorithm

--- include/AntColony.hpp
+++ include/AntColony.hpp
@@ -8,6 +8,8 @@ class AntColony {
 	private:
 
 		double pheromones_evaporation_rate_;
+		double rho_;
+		double initial_pheromones_;
 
 		Graph<double> paths_;
 		Graph<double> pheromones_;
@@ -24,7 +26,8 @@ class AntColony {
 		AntColony(const Graph<double> & paths, const uint16_t num_ants, 
 					 const double pheromones_evaporation_rate = 0.1,
 					 const double pheromones_importance = 2,
-					 const double probability_explotation_behaviour = 0.9);
+					 const double probability_explotation_behaviour = 0.9,
+					 const double rho = 0.9);
 
 		AntColony(const AntColony & other) = default;
 
--- src/AntColony.cpp
+++ src/AntColony.cpp
@@ -4,10 +4,12 @@
 AntColony :: AntColony (const Graph<double> & paths, const uint16_t num_ants,
 							   const double pheromones_evaporation_rate,
 								const double pheromones_importance,
-								const double probability_explotation_behaviour) {
+								const double probability_explotation_behaviour,
+								const double rho) {
 	paths_ = paths;
 
 	pheromones_evaporation_rate_ = pheromones_evaporation_rate;
+	rho_ = rho;
 
 	pheromones_ = Graph<doubles>(paths.num_nodes());
 
@@ -86,6 +88,37 @@ void AntColony :: local_update_pheromones(const uint32_t position, const uint32_
 	pheromones_.connect(position, new_position, new_pheromones_value);
 
 }
+
+
+void AntColony :: calculate_initial_pheromones() {
+	// we will use a greedy algorithm to calculate this parameter
+
+	// we use a greedy ant that start at the position 0
+	Ant greedy_ant(0);
+	
+	uint32_t best_city = 1;
+	double best_city_cost = std::numeric_limits<double>::infinity();
+
+	while(greedy_ant.get_path().size() < paths_.size()) {
+		
+		uint32_t ant_position = greedy_ant.position();
+		best_city_cost = std::numeric_limits<double>::infinity();
+		
+		for ( uint32_t i = 0; i < paths_.size(); i++ ) {
+			if (!greedy_ant.visited(i) && i != ant_position && paths_.cost(ant_position, i) < best_city_cost) {
+				best_city_cost = paths_.cost(ant_position, i);
+				best_city = i;
+			}
+		}
+
+		greedy_ant.move_to_position(best_city);
+
+	}
+
+	initial_pheromones_ = paths_.num_nodes() * greedy_ant.get_path_length();
+
+}
+
 
 void AntColony :: run_simulation (const uint32_t num_iterations) noexcept {
 
