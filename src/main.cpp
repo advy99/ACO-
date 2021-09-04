9c7b2bc7b3fca582d66590f732dcf8d075cd6b7c
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-05 00:14:50 +0200

    main: adjust cities to window height and width and display final path

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,53 @@
 #include "AntColony.hpp"
 
 #include "Figures/Circle.hpp"
+#include "Figures/Line.hpp"
 
 #include "TSPReader.hpp"
 
 #include <iostream>
 #include <chrono>
 
+std::vector<Point> adjust_points(const std::vector<Point> & points, const uint32_t WIDTH, const uint32_t HEIGHT) {
+	const uint32_t max_x = std::max_element(points.begin(), points.end(), 
+														[](const Point & p1, const Point & p2){
+															return p1.x < p2.x;
+														})->x;
+
+	const uint32_t max_y = std::max_element(points.begin(), points.end(), 
+														[](const Point & p1, const Point & p2){
+															return p1.y < p2.y;
+														})->y;
+	
+	double ratio_x = WIDTH / (max_x * 1.0);
+	double ratio_y = HEIGHT / (max_y * 1.0);
+
+	std::vector<Point> solution;
+	solution.resize(points.size());
+
+	for(uint32_t i = 0; i < points.size(); i++) {
+		solution[i].x = points[i].x * ratio_x;
+		solution[i].y = points[i].y * ratio_y;
+	}
+	
+
+	return solution;
+
+}
+
+
 void run_interface(int argc, char ** argv) {
 
 	const uint32_t FPS = 60;
 	const uint32_t DELAY_TIME = 1000.0 / FPS;
 
-	ACOppInterface interface("Prueba SDL", 100, 100, 640, 480, 0);
+	const uint32_t WIDTH = 640;
+	const uint32_t HEIGHT = 480;
+
+	const uint32_t CIRCLE_RADIUS = 5;
+	const uint32_t LINE_WIDTH = 2;
+
+	ACOppInterface interface("Prueba SDL", 100, 100, WIDTH, HEIGHT, 0);
 
 
 	if ( argc < 4) {
@@ -33,12 +68,25 @@ void run_interface(int argc, char ** argv) {
 
 	AntColony my_colony(paths.first, num_ants);
 
-	auto solution = my_colony.run_simulation(iterations);
+	std::pair<std::vector<uint32_t>, double> solution = my_colony.run_simulation(iterations);
 
-	for (auto & point: paths.second) {
-		std::unique_ptr<Circle> circle = std::make_unique<Circle>(point.x, point.y, 1, Color(0, 0, 200, 255), true);
+	std::vector<Point> points = adjust_points(paths.second, WIDTH - 20, HEIGHT - 20);
+
+	for (const Point & point: points) {
+		std::unique_ptr<Circle> circle = std::make_unique<Circle>(point.x, point.y, CIRCLE_RADIUS, Color(0, 0, 200, 255), true);
 		interface.add_object(std::move(circle));
 	}
+
+	auto best_path = solution.first;
+
+	for (uint32_t i = 0; i < best_path.size() - 1; i++) {
+		std::unique_ptr<Line> line = std::make_unique<Line>(points[best_path[i]].x, points[best_path[i]].y,
+																			 points[best_path[i + 1]].x, points[best_path[i + 1]].y, LINE_WIDTH, Color(0, 0, 200, 255));
+		interface.add_object(std::move(line));
+	}
+	interface.add_object(std::make_unique<Line>(points[best_path.front()].x, points[best_path.front()].y,
+															  points[best_path.back()].x, points[best_path.back()].y, LINE_WIDTH, Color(0, 0, 200, 255)));
+
 
 	while ( interface.running() ) {
 		uint32_t frame_start, frame_time;
