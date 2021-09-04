7105d486f59c7992b9222fe852a745215cb7c0c4
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-04 18:50:32 +0200

    CLI: clear main section

--- src/ACOppCLI.cpp
+++ src/ACOppCLI.cpp
@@ -1,65 +1,10 @@
 #include "AntColony.hpp"
+#include "TSPReader.hpp"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <chrono>
-
-Graph<double> read_from_file(std::string file) {
-
-	std::ifstream f(file);
-
-	if ( !f ) {
-		throw std::runtime_error("Error opening " + file);
-	}
-
-	std::string line;
-
-	std::getline(f, line);
-
-	uint32_t num_line = 0;
-	uint32_t num_cities = 0;
-	
-	double value;
-	std::vector<double> first_line;
-	std::istringstream iss_f(line);
-
-	while (!iss_f.eof()) {
-		iss_f >> value;
-		first_line.push_back(value);
-	}
-
-	num_cities = first_line.size();
-
-	Graph<double> solution(num_cities, 0);
-
-	for (unsigned int i = 0; i < first_line.size(); i++) {
-		solution.connect(0, i, first_line[i]);
-	}
-
-	std::getline(f, line);
-	num_line++;
-	
-	uint32_t num_val = 0;
-
-	while(!f.eof()) {	
-		std::istringstream iss(line);
-		num_val = 0;
-		while (!iss.eof()) {
-			iss >> value;
-			if (num_val > num_line) {
-				solution.connect(num_line, num_val, value);
-			}
-			num_val++;
-		}
-
-			
-		std::getline(f, line);
-		num_line++;
-	}
-
-	return solution;
-}
 
 
 int main (int argc, char ** argv) {
@@ -79,7 +24,7 @@ int main (int argc, char ** argv) {
 
 
 
-	Graph<double> paths = read_from_file(file);
+	auto paths = TSPReader::read_from_file(file);
 
 	std::cout << "Paths read from " << file << "\n";
 	
