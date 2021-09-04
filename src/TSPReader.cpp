38c7ed028d00643ba82f2d08d2abc406e98d1fb1
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-04 18:49:56 +0200

    TSPReader : read from tsp file or from matrix

--- src/TSPReader.cpp
+++ src/TSPReader.cpp
@@ -1,6 +1,5 @@
-#include "TSPReader"
+#include "TSPReader.hpp"
 
-#include <cmath>
 
 double TSPReader :: calculate_distance (const Point & p1, const Point & p2, const EdgeWeightTypes edge_type) {
 
@@ -20,3 +19,151 @@ double TSPReader :: calculate_distance (const Point & p1, const Point & p2, cons
 }
 
 
+std::pair<Graph<double>, std::vector<Point> > TSPReader :: read_from_file(const std::string & file) {
+	ifstream file_reader(file);
+
+	if (!file_reader) {
+		throw std::runtime_error("Could not open " + file);
+	}
+
+	Graph<double> paths;
+	std::vector<Point> points;
+	int32_t dimension;
+
+	std::string word;
+	bool file_is_tsp_valid = false;
+
+	EdgeWeightTypes edge_type = EdgeWeightTypes::UNKNOWN;
+
+	file_reader >> word;
+
+	while(!file_reader.eof() && word != "TYPE") {
+		file_reader >> word;
+	}
+
+	file_is_tsp_valid = word == "TYPE";
+
+	if (file_is_tsp_valid) {
+		// read the " : " 
+		file_reader >> word;
+
+		// read if the type is TSP
+		file_reader >> word;
+		file_is_tsp_valid = word == "TSP" && !file_reader.eof();
+	}
+
+	if (file_is_tsp_valid) {
+		// read dimensions
+		file_reader >> word;
+
+		while(!file_reader.eof() && word != "DIMENSION") {
+			file_reader >> word;
+		}
+
+		file_reader >> word;
+		file_reader >> word;
+
+		dimension = std::atoi(word.c_str());
+
+		points.resize(dimension);
+
+
+		// read edge types
+		file_reader >> word;
+
+		while(!file_reader.eof() && word != "EDGE_WEIGHT_TYPE") {
+			file_reader >> word;
+		}
+
+		file_reader >> word;
+		file_reader >> word;
+		
+		if (word == "EUC_2D") {
+			edge_type = EdgeWeightTypes::EUCLIDEAN_2D; 
+		}
+
+		// read until start of cities
+		file_reader >> word;
+
+		while(!file_reader.eof() && word != "NODE_COORD_SECTION") {
+			file_reader >> word;
+		}
+
+		for (Point & p: points) {
+			// first word is the num of the city
+			file_reader >> word;
+			file_reader >> p.x;
+			file_reader >> p.y;
+		}
+
+		paths = Graph<double>(points.size());
+
+		for (uint32_t i = 0; i < points.size() - 1; i++) {
+			paths.connect(i, i + 1, calculate_distance(points[i], points[i + 1]));
+		}
+		
+		paths.connect(0, points.size() - 1, calculate_distance(points.front(), points.back());
+
+	}
+
+
+	return std::make_pair(paths, points);
+
+}
+
+
+Graph<double> TSPReader :: read_matrix_from_file(const std::string & file) {
+
+	std::ifstream f(file);
+
+	if ( !f ) {
+		throw std::runtime_error("Error opening " + file);
+	}
+
+	std::string line;
+
+	std::getline(f, line);
+
+	uint32_t num_line = 0;
+	uint32_t num_cities = 0;
+	
+	double value;
+	std::vector<double> first_line;
+	std::istringstream iss_f(line);
+
+	while (!iss_f.eof()) {
+		iss_f >> value;
+		first_line.push_back(value);
+	}
+
+	num_cities = first_line.size();
+
+	Graph<double> solution(num_cities, 0);
+
+	for (unsigned int i = 0; i < first_line.size(); i++) {
+		solution.connect(0, i, first_line[i]);
+	}
+
+	std::getline(f, line);
+	num_line++;
+	
+	uint32_t num_val = 0;
+
+	while(!f.eof()) {	
+		std::istringstream iss(line);
+		num_val = 0;
+		while (!iss.eof()) {
+			iss >> value;
+			if (num_val > num_line) {
+				solution.connect(num_line, num_val, value);
+			}
+			num_val++;
+		}
+
+			
+		std::getline(f, line);
+		num_line++;
+	}
+
+	return solution;
+}
