67e2a5bfafcab30dda0667a98fefd9a123975769
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-05 12:43:23 +0200

    main: separate calculations of lines, and use coroutines to yield the best solution in every iteration

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,19 @@
 #include <iostream>
 #include <chrono>
 
+
+
+
+const uint32_t FPS = 60;
+const uint32_t DELAY_TIME = 1000.0 / FPS;
+
+const uint32_t WIDTH = 640;
+const uint32_t HEIGHT = 480;
+
+const uint32_t CIRCLE_RADIUS = 5;
+const uint32_t LINE_WIDTH = 2;
+
+
 std::vector<Point> adjust_points(const std::vector<Point> & points, const uint32_t WIDTH, const uint32_t HEIGHT) {
 	const uint32_t max_x = std::max_element(points.begin(), points.end(), 
 														[](const Point & p1, const Point & p2){
@@ -36,19 +49,23 @@ std::vector<Point> adjust_points(const std::vector<Point> & points, const uint32
 
 }
 
+std::vector<std::unique_ptr<Figure> > generate_lines(const std::vector<Point> & points, const std::vector<uint32_t> & best_path) {
+	std::vector<std::unique_ptr<Figure> > solution;
+
+	for (uint32_t i = 0; i < best_path.size() - 1; i++) {
+		solution.push_back(std::make_unique<Line>(points[best_path[i]].x, points[best_path[i]].y,
+															 points[best_path[i + 1]].x, points[best_path[i + 1]].y, LINE_WIDTH, Color(0, 0, 200, 255)));
+	}
+	solution.push_back(std::make_unique<Line>(points[best_path.front()].x, points[best_path.front()].y,
+															  points[best_path.back()].x, points[best_path.back()].y, LINE_WIDTH, Color(0, 0, 200, 255)));
+
+	return solution;
+}
+
 
 void run_interface(int argc, char ** argv) {
 
-	const uint32_t FPS = 60;
-	const uint32_t DELAY_TIME = 1000.0 / FPS;
-
-	const uint32_t WIDTH = 640;
-	const uint32_t HEIGHT = 480;
-
-	const uint32_t CIRCLE_RADIUS = 5;
-	const uint32_t LINE_WIDTH = 2;
-
-	ACOppInterface interface("Prueba SDL", 100, 100, WIDTH, HEIGHT, 0);
+	ACOppInterface interface("Prueba SDL", 900, 700, WIDTH, HEIGHT, 0);
 
 
 	if ( argc < 4) {
@@ -68,25 +85,25 @@ void run_interface(int argc, char ** argv) {
 
 	AntColony my_colony(paths.first, num_ants);
 
-	std::pair<std::vector<uint32_t>, double> solution = my_colony.run_simulation(iterations);
+	// std::pair<std::vector<uint32_t>, double> solution = my_colony.run_simulation(iterations);
 
 	std::vector<Point> points = adjust_points(paths.second, WIDTH - 20, HEIGHT - 20);
 
 	for (const Point & point: points) {
 		std::unique_ptr<Circle> circle = std::make_unique<Circle>(point.x, point.y, CIRCLE_RADIUS, Color(0, 0, 200, 255), true);
-		interface.add_object(std::move(circle));
+		interface.add_city(std::move(circle));
 	}
 
-	auto best_path = solution.first;
+	// auto best_path = solution.first;
 
-	for (uint32_t i = 0; i < best_path.size() - 1; i++) {
-		std::unique_ptr<Line> line = std::make_unique<Line>(points[best_path[i]].x, points[best_path[i]].y,
-																			 points[best_path[i + 1]].x, points[best_path[i + 1]].y, LINE_WIDTH, Color(0, 0, 200, 255));
-		interface.add_object(std::move(line));
-	}
-	interface.add_object(std::make_unique<Line>(points[best_path.front()].x, points[best_path.front()].y,
-															  points[best_path.back()].x, points[best_path.back()].y, LINE_WIDTH, Color(0, 0, 200, 255)));
+	int num_iterations = 0;
 
+	std::chrono::time_point<std::chrono::system_clock> last_update = std::chrono::system_clock::now();
+	std::chrono::time_point<std::chrono::system_clock> now;
+
+
+	std::coroutine_handle<ReturnObject::promise_type> generator =  my_colony.run_step_simulation();
+	ReturnObject::promise_type & promise = generator.promise();
 
 	while ( interface.running() ) {
 		uint32_t frame_start, frame_time;
@@ -94,6 +111,23 @@ void run_interface(int argc, char ** argv) {
 
 		interface.handle_events();
 		interface.render();
+			
+		now = std::chrono::system_clock::now();
+
+		auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_update);
+
+		if ( iterations > num_iterations && milliseconds.count() > 50) {
+			last_update = now;
+			
+			auto iteration_solution = promise.val_;
+
+			interface.change_lines(generate_lines(points, iteration_solution.first));
+
+			generator();
+
+			num_iterations++;
+		}
+
 
 		frame_time = SDL_GetTicks() - frame_start;
 
